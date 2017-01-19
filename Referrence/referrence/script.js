4d1849c3f7c9a76f1fe480c708d0416aa413709e
rocky <rocky1203good@gmail.com>
2017-01-20 02:34:22 +0530

    news html collpase and archive added

--- Referrence/referrence/script.js
+++ Referrence/referrence/script.js
@@ -31,4 +31,11 @@ $(document).ready(function(){
     }
     $('.panel-group').on('hidden.bs.collapse', toggleIcon);
     $('.panel-group').on('shown.bs.collapse', toggleIcon);
+    
+  var selectIds = $('#panel1,#panel2,#panel3');
+  $(function ($) {
+      selectIds.on('show.bs.collapse hidden.bs.collapse', function () {
+          $(this).prev().find('.glyphicon').toggleClass('glyphicon-plus glyphicon-minus');
+      })
+  });
 });
